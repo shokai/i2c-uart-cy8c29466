ee82d1eefa4d26cfb0528fc3f4dd4b39c6797a10
shokai <hashimoto@shokai.org>
2010-07-18 20:17:48 +0900

    put LED

--- i2c_master/i2c_master/i2c_master/main.c
+++ i2c_master/i2c_master/i2c_master/main.c
@@ -1,12 +1,18 @@
-//----------------------------------------------------------------------------
-// C main line
-//----------------------------------------------------------------------------
+// I2C-UART master
+// CY8C29466-24PXI(DIP Package)
+// PSoC Designer 5.0 + SP6
+// IMAGECRAFT C Compiler
 
 #include <m8c.h>        // part specific constants and macros
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
+#define _BV(BIT) (1<<BIT)
+#define sbi(BYTE,BIT) (BYTE |= _BV(BIT))
+#define cbi(BYTE,BIT) (BYTE &= ~_BV(BIT))
+#define LED_ON() sbi(PRT2DR, 0)
+#define LED_OFF() cbi(PRT2DR, 0)
 
 
 void main(void)
 {
-    // Insert your main routine code here.
+    LED_ON();
 }
