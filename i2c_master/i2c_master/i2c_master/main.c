a46b7d8b6e56f2885263779652bb86280ae3a907
shokai <hashimoto@shokai.org>
2010-07-18 22:32:22 +0900

    uart

--- i2c_master/i2c_master/i2c_master/main.c
+++ i2c_master/i2c_master/i2c_master/main.c
@@ -14,5 +14,14 @@
 
 void main(void)
 {
+    M8C_EnableGInt; // enable global interrupt
+    M8C_EnableIntMask(INT_MSK0, INT_MSK0_GPIO);
+    UART_1_CmdReset(); // uart init
+    UART_1_IntCntl(UART_1_ENABLE_RX_INT); // enable receive interrupt
+    UART_1_Start(UART_1_PARITY_NONE);
     LED_ON();
+
+    while(1){
+        UART_1_CPutString("run\r\n");
+    }
 }
