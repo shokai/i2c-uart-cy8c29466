1ce5e98d16449ea43e22fc7a86fa6f8eb178d3ae
shokai <hashimoto@shokai.org>
2010-07-20 07:00:24 +0900

    master-(I2C)->slave-(I2C)->master-(uart)->rs232

--- i2c_master/i2c_master/i2c_master/main.c
+++ i2c_master/i2c_master/i2c_master/main.c
@@ -4,6 +4,8 @@
 // IMAGECRAFT C Compiler
 
 #include <m8c.h>        // part specific constants and macros
+#include <I2CHW_1Common.h>
+#include <I2CHW_1Mstr.h>
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
 #define _BV(BIT) (1<<BIT)
 #define sbi(BYTE,BIT) (BYTE |= _BV(BIT))
@@ -11,12 +13,19 @@
 #define LED_ON() sbi(PRT2DR, 0)
 #define LED_OFF() cbi(PRT2DR, 0)
 #define bit_is_set(BYTE, BIT) BYTE & BIT
+#define loop_until_bit_is_set(BYTE, BIT) while(!bit_is_set(BYTE,BIT));
 #define SW_PORT PRT2DR
 #define SW_BIT _BV(2) // switch
 
+#define BUF_SIZE 64
+BYTE buf_tx[BUF_SIZE] = "AB";
+BYTE buf_rx[BUF_SIZE];
+#define SLAVE_ADDR 0
+BYTE status;
 
 void main(void)
 {
+   
     M8C_EnableGInt; // enable global interrupt
     M8C_EnableIntMask(INT_MSK0, INT_MSK0_GPIO);
     UART_1_CmdReset(); // uart init
@@ -24,9 +33,19 @@ void main(void)
     UART_1_Start(UART_1_PARITY_NONE);
     LED_ON();
     UART_1_CPutString("start");
+    I2CHW_1_Start();
+    I2CHW_1_EnableMstr();
+    I2CHW_1_EnableInt();
 
-    while(1){
-        //UART_1_CPutString("loop\r\n");
+    for(;;){
+        I2CHW_1_bWriteBytes(SLAVE_ADDR, buf_tx, BUF_SIZE, I2CHW_1_CompleteXfer);
+        while(!(I2CHW_1_bReadI2CStatus() & I2CHW_WR_COMPLETE));
+        I2CHW_1_ClrWrStatus();
+
+        I2CHW_1_fReadBytes(SLAVE_ADDR, buf_rx, BUF_SIZE, I2CHW_1_CompleteXfer);
+        while(!(I2CHW_1_bReadI2CStatus() & I2CHW_RD_COMPLETE));
+        I2CHW_1_ClrRdStatus();
+        UART_1_PutString(buf_rx);
     }
 }
 
--- i2c_slave/i2c_slave/i2c_slave/main.c
+++ i2c_slave/i2c_slave/i2c_slave/main.c
@@ -4,23 +4,46 @@
 // IMAGECRAFT C Compiler
 
 #include <m8c.h>        // part specific constants and macros
+#include <I2CHW_1Common.h>
 #include "PSoCAPI.h"    // PSoC API definitions for all User Modules
+#include <string.h>
 #define _BV(BIT) (1<<BIT)
 #define sbi(BYTE,BIT) (BYTE |= _BV(BIT))
 #define cbi(BYTE,BIT) (BYTE &= ~_BV(BIT))
 #define LED_ON() sbi(PRT2DR, 0)
 #define LED_OFF() cbi(PRT2DR, 0)
 #define bit_is_set(BYTE, BIT) BYTE & BIT
+#define loop_until_bit_is_set(BYTE, BIT) while(!bit_is_set(BYTE,BIT));
 #define SW_PORT PRT2DR
 #define SW_BIT _BV(2) // switch
+
+#define BUF_SIZE 8
+BYTE buf[BUF_SIZE];
+BYTE status;
 
 void main(void)
 {
     M8C_EnableGInt;
     M8C_EnableIntMask(INT_MSK0, INT_MSK0_GPIO);
-    LED_ON();
+    I2CHW_1_Start();
+    I2CHW_1_EnableSlave();
+    I2CHW_1_EnableInt();
+    I2CHW_1_InitRamRead(buf, BUF_SIZE);
+    I2CHW_1_InitWrite(buf, BUF_SIZE);
 
-    while(1){
+    for(;;){
+        status = I2CHW_1_bReadI2CStatus();
+        if(status & I2CHW_WR_COMPLETE){
+            I2CHW_1_ClrWrStatus();
+            buf[2] = 'C';
+            I2CHW_1_InitWrite(buf, BUF_SIZE);
+            LED_ON();
+        }
+        if(status & I2CHW_RD_COMPLETE){
+            I2CHW_1_ClrRdStatus();
+            I2CHW_1_InitRamRead(buf, BUF_SIZE);
+            LED_OFF();
+        }
     }
 }
 
@@ -33,3 +56,4 @@ void INT_GPIO(void){
         LED_OFF();
     }
 }
+
